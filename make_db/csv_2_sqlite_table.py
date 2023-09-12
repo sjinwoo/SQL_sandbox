a9b7967500db24ba59691ac85aa5b4f70c8f4a82
sjinwoo <sjw5953@naver.com>
2023-09-12 10:39:06 +0900

    Create make_db.py

---
+++ make_db/csv_2_sqlite_table.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sqlite3
+
+def commit_query(database, xlsx_path, sheet_name=None):
+    dfs = pd.read_excel(xlsx_path, sheet_name=sheet_name)
+
+    for table, df in dfs.items():
+        df.to_sql(table, database)
+    database.commit()
+
+    return
+
+
+def main():
+    db = sqlite3.connect("flat-table.sqlite")
+    commit_query(db, './make_db/복리후생.xlsx')
+    db.close()
+
+if __name__ == "__main__":
+    main()
