4aef029ba1f8ea90b461fdac72b16eed4e56ca2e
sjinwoo <sjw5953@naver.com>
2023-09-12 10:39:52 +0900

    Add Table Desc, Tool Desc

--- constants.py
+++ constants.py
@@ -1,29 +1,20 @@
-DEFAULT_SQL_PATH = "sqlite:///sfscores.sqlite"
+# sqlite:// 프로토콜 사용, /sfscores.sqlite 경로
+DEFAULT_SQL_PATH = "sqlite:///flat-table.sqlite"
 
 # Table Description
-DEFAULT_BUSINESS_TABLE_DESCRP = (
-    "This table gives information on the IDs, addresses, and other location "
-    "information for several restaurants in San Francisco. This table will "
-    "need to be referenced when users ask about specific businesses."
+DEFAULT_WELFARE_TABLE_DESCRP = (
+    "이 테이블은 기업에 있는 행사의 구분, 대상, 회사지원금 및 사우회지급금, 경조휴가, 화환 및 조화 지급 여부에 대한 정보를 제공합니다."
+    "사용자가 특정 복리후생에 대해 질의할 때 이 테이블을 참조해야 합니다."
 )
-DEFAULT_VIOLATIONS_TABLE_DESCRP = (
-    "This table gives information on which business IDs have recorded health violations, "
-    "including the date, risk, and description of each violation. The user may query "
-    "about specific businesses, whose names can be found by mapping the business_id "
-    "to the 'businesses' table."
+DEFAULT_WELFARE_BY_RANK_TABLE_DESCRP = (
+    "이 테이블은 자녀의 신분 기준에 따라 지급되는 지원금에 대한 정보를 제공합니다."
+    "이 테이블의 비고는 추가적인 지원금 또는 지원 기준에 대해 설명합니다."
+    "사용자가 자녀의 기준에 따른 복리후생에 대해 질의할 때 이 테이블을 참조해야 합니다."
 )
-DEFAULT_INSPECTIONS_TABLE_DESCRP = (
-    "This table gives information on when each business ID was inspected, including "
-    "the score, date, and type of inspection. The user may query about specific "
-    "businesses, whose names can be found by mapping the business_id to the 'businesses' table."
+DEFAULT_WELFARE_BY_STANDARD_TABLE_DESCRP = (
+    "이 테이블은 연봉직과 시급직의 구분에 따른 직급별 연 지급액, 분기 지급액에 대한 정보를 제공합니다."
+    "사용자가 구분 및 직급에 따른 복리후생에 대해 질의할 때 이 테이블을 참조해야 합니다."
 )
 
-# 
-DEFAULT_LC_TOOL_DESCRP = "Useful for when you want to answer queries about violations and inspections of businesses."
-
-# 
-DEFAULT_INGEST_DOCUMENT = (
-    "The restaurant KING-KONG had an routine unscheduled inspection on 2023/12/31. "
-    "The business achieved a score of 50. We two violations, a high risk "
-    "vermin infestation as well as a high risk food holding temperatures."
-)
+# Tool Description
+DEFAULT_LC_TOOL_DESCRP = "사내의 복리후생 및 지원에 대한 질의에 답변하고 싶을 때 유용합니다."
