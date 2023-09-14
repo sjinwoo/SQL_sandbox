d31bedcecbc1decb7ee67d44ff9d04ee04b509f5
sjinwoo <sjw5953@naver.com>
2023-09-14 16:16:54 +0900

    Add index column

--- make_db/csv_2_sqlite_table.py
+++ make_db/csv_2_sqlite_table.py
@@ -5,14 +5,16 @@ def commit_query(database, xlsx_path, sheet_name=None):
     dfs = pd.read_excel(xlsx_path, sheet_name=sheet_name)
 
     for table, df in dfs.items():
+        if table == 'WELFARE_RANK':
+            df.to_sql(table, database, index=True)
+            continue
         df.to_sql(table, database, index=False)
     database.commit()
 
     return
 
-
 def main():
-    db = sqlite3.connect("flat-table-3.sqlite")
+    db = sqlite3.connect("flat-table-final.sqlite")
     commit_query(db, './make_db/복리후생.xlsx')
     db.close()
 
